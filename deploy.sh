570d25949ce3dc975d38b7bcb6e9b6776532a047
ilyes ajroud <ilyes.ajroud@gmail.com>
2020-09-10 18:14:57 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 cd public
-if [ -n "$GITHUB_TOKEN" ]
-then
-    touch ~/.git-credentials
-    chmod 0600 ~/.git-credentials
-    echo $GITHUB_TOKEN > ~/.git-credentials
-    git config credential.helper store
-    git config user.email "ilyesAj-bot@users.noreply.github.com"
-    git config user.name "ilyesAj-bot"
-fi
+touch ~/.git-credentials
+chmod 0600 ~/.git-credentials
+echo $GITHUB_TOKEN > ~/.git-credentials
+git config credential.helper store
+git config user.email "ilyesAj-bot@users.noreply.github.com"
+git config user.name "ilyesAj-bot"
 ls 
 git remote show origin
 git add .
